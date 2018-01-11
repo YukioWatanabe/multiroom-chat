a2f6e9411218cb5b81f938b4b98b2d2bd9ad3208
Fernando Watanabe <maz_fwatanabe@uolinc.com>
2018-01-10 23:00:45 -0200

    Criado mecanismo de chat

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 const app = require('./config/server');
+const socket_io = require('socket.io');
 const port = 3000;
+const participants = [];
 
-app.listen(3000, () => {
+const server = app.listen(3000, () => {
     console.log("Server is running on port " + port);
+});
+
+const io = socket_io.listen(server);
+
+app.set('io', io);
+
+io.on('connection', (socket) => {
+
+    socket.on('new-participant', ( username ) => {
+        participants.push(username);
+        socket.emit('load-participants', participants);
+    });
+
+    socket.on('message-from-user-to-server', (data) => {
+        socket.emit('message-from-server-to-user', data);
+        socket.broadcast.emit('message-from-server-to-user', data);
+    });
 });
--- src/controllers/chat.js
+++ src/controllers/chat.js
@@ -3,14 +3,17 @@ module.exports.iniciaChat = (app, req, res) => {
     req.assert('username', 'Nome ou apelido deve ter entre 3 e 15 caracteres').len(3,15);
 
     const errors   = req.validationErrors();
-    const username = req.body;
+    const username = req.body.username;
+    const io = app.get('io');
 
     if(errors){
         app.src.controllers.index.index(app, req, res, { errors });
         return;
     }
+
+    io.emit('user-connected', { username });
     
-    res.render("chat");
+    res.render("chat", { username });
 }
 
 module.exports.chat = (app, req, res) => {
